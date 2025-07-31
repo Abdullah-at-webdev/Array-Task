fdcd041af891f14bf97a6e49cd2cf3f6250cfef6
Hasanfunn <mamunhosein99@gmail.com>
2025-07-31 08:48:37 +0600

    Just tried Out Some Array includes

---
+++ books.js
@@ -0,0 +1,9 @@
+const likedBooks = ['local-time', 'True-skills', 'Hablu-Programmers', 'Try-To-Improve', 'Javascript-Learning', 'A-Complete-Roadmap', 'Time Trap', 'Our Sociality', 'Prime World']
+console.log(likedBooks.length);
+
+if(likedBooks.includes('Javascript-Learning')){
+    console.log('Yes We Have The Book You Want. Let Us Know If You Need One')
+}
+else{
+    console.log('Sorry We Do Not Have That One. We Will Nootify You We Could Manage You One')
+}
