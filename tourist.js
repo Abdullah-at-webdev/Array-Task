4deac952261638b54591b8ee5928fd982b97d47b
Hasanfunn <mamunhosein99@gmail.com>
2025-07-31 08:28:53 +0600

    Just Worked on Some Push and pop tags

---
+++ tourist.js
@@ -0,0 +1,12 @@
+const touristSpot = ['Cox-Bazar', 'Padma', 'Quakata'];
+console.log(touristSpot);
+//Adding a New Spot with "push" tag
+touristSpot.push('Jamuna-Feature-Park');
+console.log(touristSpot);
+// Adding 2 new Spot with "push" tag
+touristSpot.push('Jaflong', 'Bandarban');
+console.log(touristSpot);
+
+// Removing the last Spot with 'pop(); tag
+touristSpot.pop();
+console.log(touristSpot);
