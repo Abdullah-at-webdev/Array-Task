bd4b6570da1d9c5a856019ff314d9321488557a3
Hasanfunn <mamunhosein99@gmail.com>
2025-07-31 08:12:07 +0600

    Just worked on some change of array elements

--- fruits.js
+++ fruits.js
@@ -1,3 +1,8 @@
 const fruits = ['Apple', 'Pine-apple', 'Grapes', 'Orange', 'Banana'];
 console.log(fruits);
-console.log(index(fruits));
+console.log(fruits.length);
+
+// fruits[2]= 'Jambura';
+// console.log(fruits);
+fruitsChangeName = fruits.with(2, 'Jambura');
+console.log(fruitsChangeName);
