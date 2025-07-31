ea1e81a792fc1894e774ca2e12d56cd4bbe9df83
Hasanfunn <mamunhosein99@gmail.com>
2025-07-31 10:12:41 +0600

    Good now I understood If there Are Array on the syntax

--- books.js
+++ books.js
@@ -1,8 +1,16 @@
 const likedBooks = ['local-time', 'True-skills', 'Hablu-Programmers', 'Try-To-Improve', 'Javascript-Learning', 'A-Complete-Roadmap', 'Time Trap', 'Our Sociality', 'Prime World']
 console.log(likedBooks.length);
 
-if(likedBooks.includes('Javascript-Learning')){
+// if(likedBooks.includes('Javascript-Learning')){
+//     console.log('Yes We Have The Book You Want. Let Us Know If You Need One')
+// }
+
+// Javascript is case sensative So You Have To write It As It is If Not I won't Work
+if(likedBooks.includes('Javascript-learning')){
     console.log('Yes We Have The Book You Want. Let Us Know If You Need One')
+}
+else if (likedBooks.includes('improve')){
+    console.log('We Got One. Do You Need One?')
 }
 else{
     console.log('Sorry We Do Not Have That One. We Will Nootify You We Could Manage You One')
---
+++ isArray.js
@@ -0,0 +1,17 @@
+const products = 'Books';
+const ages = [33, 45, 35, 11, 44, 11, 23, 33];
+console.log(ages.length);
+
+
+if (Array.isArray(ages && products)){
+    console.log('Yes These Are All Arreys Elements.')
+}
+else if (Array.isArray(ages || products)){
+    console.log('Yes One Of The Variable Has Array Elements')
+}
+else{
+    console.log('No These Are Not Array Elements')
+}
+
+const numbers = (33, 34, 25, 333, 78);
+console.log(numbers);
